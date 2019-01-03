2021f332cd2d1639fcfbd4ac2107733c523624c1
Meghan Prestemon <m.m.hares@gmail.com>
2019-01-03 00:42:39 -0800

    Create functions

--- public/index.js
+++ public/index.js
@@ -1,59 +1,58 @@
-// RANDOM QUOTE GENERATOR
-var quoteData = {}
+var imageCount = {}
+var imageOrder = []
+var imageList = []
 
-function setImageOrder () {
-    $.getJSON("https://api.forismatic.com/api/1.0/","method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
-    $(".message").html('"' + data.quoteText + '"');
+function trackImageCount (e) {
+  var element = e.target;
+  var currImage = $(element).attr("src");
 
-    if (data.quoteAuthor === "") {
-      data.quoteAuthor = "Unknown";
-    }
-    quoteData = data;
-    $(".author").html("- " + data.quoteAuthor);
+  if (currImage in imageCount) {
+    imageCount[currImage] = imageCount[currImage] + 1
+  } else {
+    imageCount[currImage] = 1
+  }
 
-    $("#getMessage").attr("disabled", false);
-
-  });
+  sortImageList(imageCount)
 }
 
-function trackImageCount () {
-  
+function sortImageList(imageCountObj) {
+  // convert object into array
+	for(var key in imageCountObj)
+		if(imageCountObj.hasOwnProperty(key))
+			imageList.push([key, imageCountObj[key]]);
+
+	// sort items by value
+	imageList.sort(function(a, b)
+	{
+	  return a[1]-b[1]; // compare numbers
+	});
+}
+
+function setImageOrder (e) {
+  var element = e.target;
+  var currImage = $(element).attr("src");
+
+  if (!imageOrder.includes(currImage)) {
+    imageOrder.push(currImage)
+  }
+
+  if (imageOrder.length == 8) {
+    localStorage.setItem("imageOrder", JSON.stringify(imageOrder));
+  }
 }
 
 // INITIALIZERS
 
 $(document).ready(function () {
 
-  $(".image").on("click", function () {
-    setImageOrder();
-    trackImageCount();
-  });
+  //set image load order
+  if (imageOrder in localStorage) {
+    var storedImageOrder = JSON.parse(localStorage.getItem("imageOrder"));
+  } //else { load default order }
 
-  $(".twitter-share-button").on("click", function (e) {
-    e.preventDefault();
-    var twitterUrl = `https://twitter.com/intent/tweet?text="${encodeURIComponent(quoteData.quoteText)}" - ${encodeURIComponent(quoteData.quoteAuthor)}&hashtags=quotes`;
-    if ((`"${quoteData.quoteText}" - ${quoteData.quoteAuthor} #quotes`).length > 140) {
-      var confirm = window.confirm("This quote is over 140 characters! Would you still like to tweet this quote?");
-      if (confirm) {
-        window.open(twitterUrl);
-      }
-    } else {
-      window.open(twitterUrl);
-    }
-
-  });
-
-  getData();
-
-  // YOGA STUDIO LOCATOR
-  $('#search-form').submit(event => {
-    event.preventDefault();
-    const searchTerm = $('#search-term').val();
-    getPlaces(searchTerm)
-    .then(response => response.json())
-    .then((places) => {
-      createMapMarkers(places);
-    })
+  $(".image").on("click", function (e) {
+    trackImageCount(e);
+    setImageOrder(e);
   });
 
 });

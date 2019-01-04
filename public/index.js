124a1a9d17a6d126874c4279c0d9e5ff6fa5a22e
Meghan Prestemon <m.m.hares@gmail.com>
2019-01-03 20:15:13 -0800

    Refactor code and add comments

--- public/index.js
+++ public/index.js
@@ -1,80 +1,64 @@
-var imageCount = {}
-var imageOrder = []
+var imageCount = {};
+var imageOrder = [];
 
 function sortImageList(imageCountObj) {
-  var imageList = []
-  // convert object into array
-	for(var key in imageCountObj)
-		if(imageCountObj.hasOwnProperty(key))
+
+  //Create and sort list of clicked images in descending order
+  var imageList = [];
+	for(key in imageCountObj)
+		if(imageCountObj.hasOwnProperty(key)) {
 			imageList.push([key, imageCountObj[key]]);
+    };
+	imageList.sort((a, b)	=> b[1]-a[1]);
 
-	// sort items by value
-	imageList.sort(function(a, b)
-	{
-	  return b[1]-a[1]; // compare numbers
-	});
-
-  //PUSH IMAGE LIST TO HTML
-  $('#imageListByCount').empty();
-  var ul = document.createElement('ul');
-  document.getElementById('imageListByCount').appendChild(ul);
-  imageList.forEach(function (image) {
-      var li = document.createElement('li');
-      ul.appendChild(li);
-
-      li.innerHTML += image[0];
-  });
-}
+  //Create and update <ul> tracking most clicked images
+  $("#imageListByCount").empty();
+  var newUl = $("#imageListByCount").append("<ul></ul>");
+  imageList.forEach(image => newUl.append("<li>" + image[0] + "</li>"));
+};
 
 function trackImageCount (e) {
   var element = e.target;
   var currImage = $(element).attr("src");
 
-  if (currImage in imageCount) {
-    imageCount[currImage] = imageCount[currImage] + 1
-  } else {
-    imageCount[currImage] = 1
-  }
+  //Increase image click count in imageCount object
+  currImage in imageCount ? imageCount[currImage] ++ : imageCount[currImage] = 1;
 
   sortImageList(imageCount)
-}
+};
 
 function setImageOrder (e) {
   var element = e.target;
   var currImage = $(element).attr("src");
 
+  //Add image to imageOrder array if it's not there already, so that it will render in order on page load
   if (!imageOrder.includes(currImage)) {
-    imageOrder.push(currImage)
-  }
+    imageOrder.push(currImage);
+  };
 
+  //If all images have been clicked, store them in localStorage tobe rendered in order on page load
   if (imageOrder.length == 8) {
     localStorage.setItem("imageOrder", JSON.stringify(imageOrder));
-  }
-}
+  };
+};
 
 // INITIALIZERS
+$(document).ready(() => {
 
-$(document).ready(function () {
-
-  //set image load order
+  //If image order has been stored in localStorage, render in saved order
   if (localStorage.getItem("imageOrder") !== null) {
-    console.log('******here********');
     var storedImageOrder = JSON.parse(localStorage.getItem("imageOrder"));
-    console.log(storedImageOrder);
-    document.getElementById("image0").src = storedImageOrder[0];
-    document.getElementById("image1").src = storedImageOrder[1];
-    document.getElementById("image2").src = storedImageOrder[2];
-    document.getElementById("image3").src = storedImageOrder[3];
-    document.getElementById("image4").src = storedImageOrder[4];
-    document.getElementById("image5").src = storedImageOrder[5];
-    document.getElementById("image6").src = storedImageOrder[6];
-    document.getElementById("image7").src = storedImageOrder[7];
+    var i = 0;
+    storedImageOrder.forEach(image => {
+      $("#image"+i).attr("src", storedImageOrder[i]);
+      i++;
+    });
     localStorage.removeItem("imageOrder");
-  }
+  };
 
-  $(".image").on("click", function (e) {
+  //Create click handlers
+  $(".image").on("click", (e) => {
     trackImageCount(e);
     setImageOrder(e);
   });
-
 });
